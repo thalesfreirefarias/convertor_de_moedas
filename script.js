6c96ac581b42e1edafe81de1857f01d4e34bce9f
Thales Farias <134720323+thalesfreirefarias@users.noreply.github.com>
2023-11-21 12:05:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-function conversaoDolar(){
-  // Get the values from the input fields
-  var real = document.getElementById('real').value;
-  var dolar = document.getElementById('dolar').value;
+function conversaoDolar() {
+  // Get the values from the input fields and parse them as floats
+  var real = parseFloat(document.getElementById('real').value);
+  var dolar = parseFloat(document.getElementById('dolar').value);
+
+  // Check if the input values are valid numbers
+  if (isNaN(real) || isNaN(dolar)) {
+    alert('Por favor, insira valores numéricos válidos.');
+    return;
+  }
 
   // Calculate the conversion based on the exchange rate and round to 2 decimal places
   var cotacao = (real * dolar).toFixed(2);
   
-  // Display the result in an element with the id 'cotação'
- document.getElementById('cotacao').innerHTML = "O valor do dólar é: " + cotacao;
+  // Display the result in an element with the id 'cotacao'
+  document.getElementById('cotacao').innerHTML = "O valor do dólar é: " + cotacao;
 
   // Clear the input fields
   limpaCampos();
 }
 
-function limpaCampos(){
+function limpaCampos() {
   // Clear the values in the input fields with the ids 'real' and 'dolar'
   document.getElementById('real').value = '';
   document.getElementById('dolar').value = '';
